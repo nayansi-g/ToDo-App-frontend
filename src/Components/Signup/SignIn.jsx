794f714e73f60a0a931ce3a058034ef26ad8b821
Nayansi <nayansigupta29@gmail.com>
2025-02-27 22:58:48 +0530

    Login typed

--- src/Components/Signup/SignIn.jsx
+++ src/Components/Signup/SignIn.jsx
@@ -50,7 +50,7 @@ const SignIn = () => {
             <div className='container'>
                 <div className='row'>
                     <div className='col-lg-4 column  col-left d-flex justify-content-center align-items-center'>
-                        <Headercomp first="Sign" second="In" />
+                        <Headercomp first="Log" second="In" />
                     </div>
                     <div className='col-lg-8 column d-flex justify-content-center align-items-center'>
                         <div className='d-flex flex-column w-100 p-5'>
@@ -62,7 +62,7 @@ const SignIn = () => {
                                 type="password" name='password'
                                 placeholder='Enter Your Password' ref={password} />
 
-                            <button className='btn-signup p-2' onClick={submitForm}>SignIn</button>
+                            <button className='btn-signup p-2' onClick={submitForm}>LogIn</button>
                         </div>
                     </div>
 
