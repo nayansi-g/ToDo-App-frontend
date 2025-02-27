4479a6add2d1af78e680766f680caa1158b3119e
Nayansi <nayansigupta29@gmail.com>
2025-02-27 23:41:43 +0530

    signup css updated

--- src/Components/Signup/SignUp.jsx
+++ src/Components/Signup/SignUp.jsx
@@ -54,7 +54,7 @@ const SignUp = () => {
                     </div>
                     <div className='col-lg-8 column d-flex justify-content-center align-items-center'>
                         <div className='d-flex flex-column w-100 p-5'>
-                            <form className='d-flex flex-column w-100 p-5' onSubmit={submitForm}>
+                            <form className='d-flex flex-column w-100' onSubmit={submitForm}>
                                 <input className='p-2 my-3 input-signup'
                                     type="email" name='email' ref={email}
                                     placeholder='Enter Your Email' />
